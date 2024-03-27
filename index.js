163063dc794d68ff6011775a557506665809742c
ibrahimmuhaned <ibrahimmuhaned999@gmail.com>
2024-03-27 22:39:49 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  function wrapAdjective(flair = '*') {
+    return function(adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  console.log(saturdayFun());
+  console.log(saturdayFun('play soccer')); 
+  console.log(mondayWork()); 
+  console.log(mondayWork('read a book'));
+  const highlightStar = wrapAdjective('*');
+  console.log(highlightStar('awesome'));
+  const highlightPipe = wrapAdjective('||');
+  console.log(highlightPipe('fantastic')); 
